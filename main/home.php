a08db6035b159051e5ead321da8f19635fc7c2fb
devkev95 <devkev95@gmail.com>
2016-09-23 01:09:13 -0600

    Arreglo de pantalla de crear partida

--- main/home.php
+++ main/home.php
@@ -84,7 +84,7 @@
         </a>
        </div>
        <div id="c-forms" class="accordion-body collapse in"><div class="accordion-inner">
-        <a href="tabla_recursos.php" class="sbtn sbtn-default active">Ver Recursos</a>
+        <a href="tabla_recursos.php" class="sbtn sbtn-default">Ver Recursos</a>
         <a href="ingresar.php" class="sbtn sbtn-default">Agregar Nuevo Recurso</a>
        </div></div>
       </div>
