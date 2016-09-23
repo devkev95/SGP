a6040b984ef341a93054f5b9610bbd641412d857
kaguilar94m <kaguilar94m@gmail.com>
2016-09-22 21:58:35 -0600

    Archivos crear partida

---
+++ main/insertarLR.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$cod=$_GET["cod"];
+$cant=$_GET["cantidad"];
+
+ error_reporting(0);
+
+ $link=mysql_connect("localhost","sgp_user","56p_2016");
+ 
+ if ($link) {
+  mysql_select_db("sgp_system", $link);
+  # code...
+ }
+
+
+
+
+$sql = mysql_query("select * from recurso where codigo='$cod' ");
+
+        while ($row = mysql_fetch_array($sql)) {
+
+
+     $valor=$row['costoDirecto'];
+     echo $valor;
+
+	$sub=($cant*$valor);
+
+
+
+        $sql3=mysql_query("INSERT INTO linearecurso (id, numero, codigo, cantidad, subTotal) VALUES (null, 1,'$cod',$cant, $sub)");
+
+}
+
+header("Location:crear_partida.php");
+
+
+
+?>
