b34cfb3477c47f8d57c8ee2e0ffaf8feeb42a986
yohalmito4 <yohalmito4@gmail.com>
2016-10-03 15:20:47 -0500

    cambios minimos

--- main/crear_partida.php
+++ main/crear_partida.php
@@ -103,7 +103,7 @@ error_reporting(0);
          <span class="caret"></span>
         </a>
        </div>
-       <div id="c-forms" class="accordion-body collapse in"><div class="accordion-inner">
+       <div id="c-forms" class="accordion-body collapse"><div class="accordion-inner">
         <a href="tabla_recursos.php" class="sbtn sbtn-default">Ver Recursos</a>
         <a href="ingresar.php" class="sbtn sbtn-default">Agregar Nuevo Recurso</a>
        </div></div>
