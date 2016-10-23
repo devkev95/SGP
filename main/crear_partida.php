2c4808c3895f0eda00b3ada847e763ab90a5efb0
katyaHerrera <katya_yan.herrera@hotmail.com>
2016-10-23 10:50:12 -0600

    cambio de conexion
    de myqsl a mysqli en crear partida

--- main/crear_partida.php
+++ main/crear_partida.php
@@ -14,10 +14,10 @@ session_write_close();
 
 error_reporting(0);
 
-$link=mysql_connect("localhost","sgp_user","56p_2016");
+$link=mysqli_connect("localhost","sgp_user","56p_2016");
 
 if ($link) {
-    mysql_select_db("sgp_system", $link);
+    mysqli_select_db("sgp_system", $link);
     # code...
 }
 
